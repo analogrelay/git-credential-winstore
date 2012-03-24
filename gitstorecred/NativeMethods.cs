cc3d7258dd504062fdc9a6952623a2d6d40bfd9b
Andrew Nurse <andrew@andrewnurse.net>
2012-03-23 17:04:16 -0700

    Initial Commit

---
+++ gitstorecred/NativeMethods.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace gitstorecred
+{
+    internal static class NativeMethods
+    {
+        [DllImport("credui.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern Boolean CredPackAuthenticationBuffer(
+            int dwFlags,
+            string pszUserName,
+            string pszPassword,
+            ref IntPtr pPackedCredentials,
+            ref uint pcbPackedCredentials);
+
+        [DllImport("credui.dll", CharSet = CharSet.Unicode)]
+        public static extern bool CredUnPackAuthenticationBuffer(
+            int dwFlags,
+            IntPtr pAuthBuffer,
+            uint cbAuthBuffer,
+            StringBuilder pszUserName,
+            ref int pcchUserName,
+            StringBuilder pszDomainName, 
+            ref int pcchDomainName,
+            StringBuilder pszPassword, 
+            ref int pcchPassword);
+
+        [DllImport("credui.dll", CharSet = CharSet.Unicode)]
+        public static extern CredUIReturnCodes CredUIPromptForWindowsCredentials(ref CREDUI_INFO notUsedHere,
+            int authError,
+            ref IntPtr authPackage,
+            IntPtr InAuthBuffer,
+            uint InAuthBufferSize,
+            out IntPtr refOutAuthBuffer,
+            out uint refOutAuthBufferSize,
+            ref bool fSave,
+            PromptForWindowsCredentialsFlags flags);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct CREDUI_INFO
+        {
+            public int cbSize;
+            public IntPtr hwndParent;
+            public string pszMessageText;
+            public string pszCaptionText;
+            public IntPtr hbmBanner;
+        }
+
+        public enum PromptForWindowsCredentialsFlags
+        {
+            /// <summary>
+            /// The caller is requesting that the credential provider return the user name and password in plain text.
+            /// This value cannot be combined with SECURE_PROMPT.
+            /// </summary>
+            CREDUIWIN_GENERIC = 0x1,
+            /// <summary>
+            /// The Save check box is displayed in the dialog box.
+            /// </summary>
+            CREDUIWIN_CHECKBOX = 0x2,
+            /// <summary>
+            /// Only credential providers that support the authentication package specified by the authPackage parameter should be enumerated.
+            /// This value cannot be combined with CREDUIWIN_IN_CRED_ONLY.
+            /// </summary>
+            CREDUIWIN_AUTHPACKAGE_ONLY = 0x10,
+            /// <summary>
+            /// Only the credentials specified by the InAuthBuffer parameter for the authentication package specified by the authPackage parameter should be enumerated.
+            /// If this flag is set, and the InAuthBuffer parameter is NULL, the function fails.
+            /// This value cannot be combined with CREDUIWIN_AUTHPACKAGE_ONLY.
+            /// </summary>
+            CREDUIWIN_IN_CRED_ONLY = 0x20,
+            /// <summary>
+            /// Credential providers should enumerate only administrators. This value is intended for User Account Control (UAC) purposes only. We recommend that external callers not set this flag.
+            /// </summary>
+            CREDUIWIN_ENUMERATE_ADMINS = 0x100,
+            /// <summary>
+            /// Only the incoming credentials for the authentication package specified by the authPackage parameter should be enumerated.
+            /// </summary>
+            CREDUIWIN_ENUMERATE_CURRENT_USER = 0x200,
+            /// <summary>
+            /// The credential dialog box should be displayed on the secure desktop. This value cannot be combined with CREDUIWIN_GENERIC.
+            /// Windows Vista: This value is not supported until Windows Vista with SP1.
+            /// </summary>
+            CREDUIWIN_SECURE_PROMPT = 0x1000,
+            /// <summary>
+            /// The credential provider should align the credential BLOB pointed to by the refOutAuthBuffer parameter to a 32-bit boundary, even if the provider is running on a 64-bit system.
+            /// </summary>
+            CREDUIWIN_PACK_32_WOW = 0x10000000,
+        }
+
+        public enum CredUIReturnCodes
+        {
+            NO_ERROR = 0,
+            ERROR_CANCELLED = 1223,
+            ERROR_NO_SUCH_LOGON_SESSION = 1312,
+            ERROR_NOT_FOUND = 1168,
+            ERROR_INVALID_ACCOUNT_NAME = 1315,
+            ERROR_INSUFFICIENT_BUFFER = 122,
+            ERROR_INVALID_PARAMETER = 87,
+            ERROR_INVALID_FLAGS = 1004,
+        }
+    }
+}
---
+++ gitstorecred/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace gitstorecred
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parse our args
+
+            // If the username was specified, pack it
+            Tuple<IntPtr, uint> inBuf = Tuple.Create<IntPtr, uint>(IntPtr.Zero, 0);
+            //if(!String.IsNullOrEmpty(options.User)) {
+            //    inBuf = PackUser(options.User) ?? inBuf;
+            //}
+
+            NativeMethods.CREDUI_INFO info = new NativeMethods.CREDUI_INFO()
+            {
+                hbmBanner = IntPtr.Zero,
+                hwndParent = IntPtr.Zero,
+                pszCaptionText = "Git Credentials",
+                pszMessageText = "Enter your credentials for ..."
+            };
+            info.cbSize = Marshal.SizeOf(info);
+
+            IntPtr outBuf;
+            uint outBufSize;
+            bool save = true;
+            IntPtr authPackage = IntPtr.Zero;
+            NativeMethods.CredUIReturnCodes ret = NativeMethods.CredUIPromptForWindowsCredentials(
+                ref info,
+                authError: 0,
+                authPackage: ref authPackage,
+                InAuthBuffer: inBuf.Item1,
+                InAuthBufferSize: inBuf.Item2,
+                refOutAuthBuffer: out outBuf,
+                refOutAuthBufferSize: out outBufSize,
+                fSave: ref save,
+                flags: NativeMethods.PromptForWindowsCredentialsFlags.CREDUIWIN_GENERIC |
+                       NativeMethods.PromptForWindowsCredentialsFlags.CREDUIWIN_CHECKBOX);
+            if (ret != NativeMethods.CredUIReturnCodes.NO_ERROR)
+            {
+                Console.Error.WriteLine("Error Prompting for Credentials: " + ret.ToString());
+                Environment.Exit((int)ret);
+                return;
+            }
+
+            // Unpack the credential and write the password to stdout
+            int userNameSize = 255;
+            int domainNameSize = 255;
+            int passwordSize = 255;
+            StringBuilder userName = new StringBuilder(userNameSize);
+            StringBuilder domainName = new StringBuilder(domainNameSize);
+            StringBuilder password = new StringBuilder(passwordSize);
+            if (!NativeMethods.CredUnPackAuthenticationBuffer(
+                dwFlags: 0x00,
+                pAuthBuffer: outBuf,
+                cbAuthBuffer: outBufSize,
+                pszUserName: userName,
+                pcchUserName: ref userNameSize,
+                pszDomainName: domainName,
+                pcchDomainName: ref domainNameSize,
+                pszPassword: password,
+                pcchPassword: ref passwordSize))
+            {
+                Console.Error.WriteLine("Error unpacking credentials");
+                return;
+            }
+        }
+
+        private static Tuple<IntPtr, uint> PackUser(string username)
+        {
+            uint size = 0;
+            IntPtr packed = IntPtr.Zero;
+            if (!NativeMethods.CredPackAuthenticationBuffer(
+                dwFlags: 0x00,
+                pszUserName: username,
+                pszPassword: String.Empty,
+                pPackedCredentials: ref packed,
+                pcbPackedCredentials: ref size))
+            {
+                Console.Error.WriteLine("Error packing credentials!");
+                return null;
+            }
+            return Tuple.Create(packed, size);
+        }
+    }
+}
