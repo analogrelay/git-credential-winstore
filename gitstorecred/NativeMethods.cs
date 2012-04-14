1fa025842702e2ce7f08d7c562cebe21ee0ed11f
anurse <andrew@andrewnurse.net>
2012-04-13 17:07:17 -0700

    Got it working!

--- gitstorecred/NativeMethods.cs
+++ gitstorecred/NativeMethods.cs
@@ -8,36 +8,18 @@ namespace gitstorecred
 {
     internal static class NativeMethods
     {
-        [DllImport("credui.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-        public static extern Boolean CredPackAuthenticationBuffer(
-            int dwFlags,
-            string pszUserName,
-            string pszPassword,
-            ref IntPtr pPackedCredentials,
-            ref uint pcbPackedCredentials);
-
         [DllImport("credui.dll", CharSet = CharSet.Unicode)]
-        public static extern bool CredUnPackAuthenticationBuffer(
-            int dwFlags,
-            IntPtr pAuthBuffer,
-            uint cbAuthBuffer,
-            StringBuilder pszUserName,
-            ref int pcchUserName,
-            StringBuilder pszDomainName, 
-            ref int pcchDomainName,
-            StringBuilder pszPassword, 
-            ref int pcchPassword);
-
-        [DllImport("credui.dll", CharSet = CharSet.Unicode)]
-        public static extern CredUIReturnCodes CredUIPromptForWindowsCredentials(ref CREDUI_INFO notUsedHere,
-            int authError,
-            ref IntPtr authPackage,
-            IntPtr InAuthBuffer,
-            uint InAuthBufferSize,
-            out IntPtr refOutAuthBuffer,
-            out uint refOutAuthBufferSize,
-            ref bool fSave,
-            PromptForWindowsCredentialsFlags flags);
+        public static extern CredUIReturnCodes CredUIPromptForCredentials(
+            ref CREDUI_INFO creditUR,
+            string targetName,
+            IntPtr reserved1,
+            int iError,
+            StringBuilder userName,
+            int maxUserName,
+            StringBuilder password,
+            int maxPassword,
+            [MarshalAs(UnmanagedType.Bool)] ref bool pfSave,
+            CREDUI_FLAGS flags);
 
         [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
         public struct CREDUI_INFO
@@ -49,45 +31,26 @@ namespace gitstorecred
             public IntPtr hbmBanner;
         }
 
-        public enum PromptForWindowsCredentialsFlags
+        [Flags]
+        public enum CREDUI_FLAGS
         {
-            /// <summary>
-            /// The caller is requesting that the credential provider return the user name and password in plain text.
-            /// This value cannot be combined with SECURE_PROMPT.
-            /// </summary>
-            CREDUIWIN_GENERIC = 0x1,
-            /// <summary>
-            /// The Save check box is displayed in the dialog box.
-            /// </summary>
-            CREDUIWIN_CHECKBOX = 0x2,
-            /// <summary>
-            /// Only credential providers that support the authentication package specified by the authPackage parameter should be enumerated.
-            /// This value cannot be combined with CREDUIWIN_IN_CRED_ONLY.
-            /// </summary>
-            CREDUIWIN_AUTHPACKAGE_ONLY = 0x10,
-            /// <summary>
-            /// Only the credentials specified by the InAuthBuffer parameter for the authentication package specified by the authPackage parameter should be enumerated.
-            /// If this flag is set, and the InAuthBuffer parameter is NULL, the function fails.
-            /// This value cannot be combined with CREDUIWIN_AUTHPACKAGE_ONLY.
-            /// </summary>
-            CREDUIWIN_IN_CRED_ONLY = 0x20,
-            /// <summary>
-            /// Credential providers should enumerate only administrators. This value is intended for User Account Control (UAC) purposes only. We recommend that external callers not set this flag.
-            /// </summary>
-            CREDUIWIN_ENUMERATE_ADMINS = 0x100,
-            /// <summary>
-            /// Only the incoming credentials for the authentication package specified by the authPackage parameter should be enumerated.
-            /// </summary>
-            CREDUIWIN_ENUMERATE_CURRENT_USER = 0x200,
-            /// <summary>
-            /// The credential dialog box should be displayed on the secure desktop. This value cannot be combined with CREDUIWIN_GENERIC.
-            /// Windows Vista: This value is not supported until Windows Vista with SP1.
-            /// </summary>
-            CREDUIWIN_SECURE_PROMPT = 0x1000,
-            /// <summary>
-            /// The credential provider should align the credential BLOB pointed to by the refOutAuthBuffer parameter to a 32-bit boundary, even if the provider is running on a 64-bit system.
-            /// </summary>
-            CREDUIWIN_PACK_32_WOW = 0x10000000,
+            INCORRECT_PASSWORD = 0x1,
+            DO_NOT_PERSIST = 0x2,
+            REQUEST_ADMINISTRATOR = 0x4,
+            EXCLUDE_CERTIFICATES = 0x8,
+            REQUIRE_CERTIFICATE = 0x10,
+            SHOW_SAVE_CHECK_BOX = 0x40,
+            ALWAYS_SHOW_UI = 0x80,
+            REQUIRE_SMARTCARD = 0x100,
+            PASSWORD_ONLY_OK = 0x200,
+            VALIDATE_USERNAME = 0x400,
+            COMPLETE_USERNAME = 0x800,
+            PERSIST = 0x1000,
+            SERVER_CREDENTIAL = 0x4000,
+            EXPECT_CONFIRMATION = 0x20000,
+            GENERIC_CREDENTIALS = 0x40000,
+            USERNAME_TARGET_CREDENTIALS = 0x80000,
+            KEEP_USERNAME = 0x100000,
         }
 
         public enum CredUIReturnCodes
--- gitstorecred/Program.cs
+++ gitstorecred/Program.cs
@@ -3,45 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 namespace gitstorecred
 {
     class Program
     {
+        static Regex GitRequestRegex = new Regex(@"^(?<req>(Username)|(Password)) for '(?<url>[^']*)':\s*$");
+
         static void Main(string[] args)
         {
-            // Parse our args
+            // Parse a line off standard in
+            string line = Console.ReadLine();
+            Match m = GitRequestRegex.Match(line, 0);
+            string request = m.Groups["req"].Value;
+            Uri url = (new Uri(m.Groups["url"].Value));
 
-            // If the username was specified, pack it
-            Tuple<IntPtr, uint> inBuf = Tuple.Create<IntPtr, uint>(IntPtr.Zero, 0);
-            //if(!String.IsNullOrEmpty(options.User)) {
-            //    inBuf = PackUser(options.User) ?? inBuf;
-            //}
+            // Extract the URL without user name to use as realm
+            string realm = url.GetComponents(
+                UriComponents.SchemeAndServer | 
+                UriComponents.Port | 
+                UriComponents.PathAndQuery, 
+                UriFormat.Unescaped);
 
-            NativeMethods.CREDUI_INFO info = new NativeMethods.CREDUI_INFO()
-            {
-                hbmBanner = IntPtr.Zero,
-                hwndParent = IntPtr.Zero,
+            // Try to get a credential
+
+            bool save = true;
+            int userNameSize = 255;
+            int domainNameSize = 255;
+            int passwordSize = 255;
+            StringBuilder userName = new StringBuilder(userNameSize);
+            StringBuilder domainName = new StringBuilder(domainNameSize);
+            StringBuilder password = new StringBuilder(passwordSize);
+
+            NativeMethods.CREDUI_FLAGS flags = 
+                NativeMethods.CREDUI_FLAGS.EXCLUDE_CERTIFICATES |
+                NativeMethods.CREDUI_FLAGS.GENERIC_CREDENTIALS |
+                NativeMethods.CREDUI_FLAGS.SHOW_SAVE_CHECK_BOX;
+            if(String.Equals("Password", request, StringComparison.OrdinalIgnoreCase) && !String.IsNullOrEmpty(url.UserInfo)) {
+                userName.Append(url.UserInfo);
+                flags |= NativeMethods.CREDUI_FLAGS.KEEP_USERNAME;
+            }
+
+            NativeMethods.CREDUI_INFO info = new NativeMethods.CREDUI_INFO() {
                 pszCaptionText = "Git Credentials",
-                pszMessageText = "Enter your credentials for ..."
+                pszMessageText = "Enter your credentials for " + realm,
             };
             info.cbSize = Marshal.SizeOf(info);
 
-            IntPtr outBuf;
-            uint outBufSize;
-            bool save = true;
-            IntPtr authPackage = IntPtr.Zero;
-            NativeMethods.CredUIReturnCodes ret = NativeMethods.CredUIPromptForWindowsCredentials(
+            NativeMethods.CredUIReturnCodes ret = NativeMethods.CredUIPromptForCredentials(
                 ref info,
-                authError: 0,
-                authPackage: ref authPackage,
-                InAuthBuffer: inBuf.Item1,
-                InAuthBufferSize: inBuf.Item2,
-                refOutAuthBuffer: out outBuf,
-                refOutAuthBufferSize: out outBufSize,
-                fSave: ref save,
-                flags: NativeMethods.PromptForWindowsCredentialsFlags.CREDUIWIN_GENERIC |
-                       NativeMethods.PromptForWindowsCredentialsFlags.CREDUIWIN_CHECKBOX);
+                targetName: "gitstorecred_" + realm,
+                reserved1: IntPtr.Zero,
+                iError: 0,
+                userName: userName,
+                maxUserName: 255,
+                password: password,
+                maxPassword: 255,
+                pfSave: ref save,
+                flags: flags);
             if (ret != NativeMethods.CredUIReturnCodes.NO_ERROR)
             {
                 Console.Error.WriteLine("Error Prompting for Credentials: " + ret.ToString());
@@ -49,44 +69,11 @@ namespace gitstorecred
                 return;
             }
 
-            // Unpack the credential and write the password to stdout
-            int userNameSize = 255;
-            int domainNameSize = 255;
-            int passwordSize = 255;
-            StringBuilder userName = new StringBuilder(userNameSize);
-            StringBuilder domainName = new StringBuilder(domainNameSize);
-            StringBuilder password = new StringBuilder(passwordSize);
-            if (!NativeMethods.CredUnPackAuthenticationBuffer(
-                dwFlags: 0x00,
-                pAuthBuffer: outBuf,
-                cbAuthBuffer: outBufSize,
-                pszUserName: userName,
-                pcchUserName: ref userNameSize,
-                pszDomainName: domainName,
-                pcchDomainName: ref domainNameSize,
-                pszPassword: password,
-                pcchPassword: ref passwordSize))
-            {
-                Console.Error.WriteLine("Error unpacking credentials");
-                return;
+            if(String.Equals("Username", request, StringComparison.OrdinalIgnoreCase)) {
+                Console.WriteLine(userName.ToString());
+            } else if(String.Equals("Password", request, StringComparison.OrdinalIgnoreCase)) {
+                Console.WriteLine(password.ToString());
             }
-        }
-
-        private static Tuple<IntPtr, uint> PackUser(string username)
-        {
-            uint size = 0;
-            IntPtr packed = IntPtr.Zero;
-            if (!NativeMethods.CredPackAuthenticationBuffer(
-                dwFlags: 0x00,
-                pszUserName: username,
-                pszPassword: String.Empty,
-                pPackedCredentials: ref packed,
-                pcbPackedCredentials: ref size))
-            {
-                Console.Error.WriteLine("Error packing credentials!");
-                return null;
-            }
-            return Tuple.Create(packed, size);
         }
     }
 }
